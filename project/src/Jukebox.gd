726feb94291c8f31be33cb9ea4810e10e3b8b553
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-08-15 16:57:31 -0400

    Add title music song

--- project/src/Jukebox.gd
+++ project/src/Jukebox.gd
@@ -1,3 +1,16 @@
 extends AudioStreamPlayer
 
-# No implementation yet.
+const _SONGS = {
+	
+}
+
+var title_theme = preload("res://assets/music/gameplay3.ogg")
+var game_theme = preload("res://assets/music/birds.ogg")
+
+func play_title_theme():
+	stream = title_theme
+	play()
+	
+func play_game_theme():
+	stream = game_theme
+	play()
--- project/src/screens/MainMenu.gd
+++ project/src/screens/MainMenu.gd
@@ -6,10 +6,12 @@ onready var _fullscreen_toggle = $VBoxContainer/HBoxContainer/FullscreenToggle
 
 
 func _ready():
+	Jukebox.play_title_theme()
 	_fullscreen_toggle.pressed = OS.window_fullscreen
 	
 
 func _on_PlayButton_pressed():
+	Jukebox.play_game_theme()
 	var _ignored_result = get_tree().change_scene_to(_GameplayLevel)
 
 
