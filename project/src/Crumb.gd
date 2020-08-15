e8c9510e3126cc18a1ac59dc5e11aaf61f47acad
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-08-15 15:14:19 -0400

    Make crumbs disappear after some time

--- project/src/Crumb.gd
+++ project/src/Crumb.gd
@@ -4,13 +4,22 @@ extends Node2D
 
 # Indicates if this crumb is in the air or on the ground
 var on_ground := true
+var color := Color.yellow
+
+export var lifetime_variance = .5
 
 onready var _radius = $CollisionShape2D.shape.radius
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
+	$LifetimeTimer.wait_time += rand_range(-lifetime_variance, lifetime_variance)
+	$LifetimeTimer.start()
 	update()
 
 
 func _draw():
-	draw_circle(Vector2.ZERO, _radius, Color.yellow)
+	draw_circle(Vector2.ZERO, _radius, color)
+
+
+func _on_LifetimeTimer_timeout():
+	queue_free()	
